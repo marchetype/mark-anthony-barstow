565d99e5decd87fc32081da9d91d50991b7cfcc1
Mark Barstow <114883447+marchetype@users.noreply.github.com>
2023-06-01 13:15:36 -0400

    working on rendering different info to the same page... instead of linking to a different one

--- mark-anthony-barstow-client/src/components/pages/Home.js
+++ mark-anthony-barstow-client/src/components/pages/Home.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import backgroundImage from '../../assets/images/homepage.jpg';
+import React, { useState } from 'react';
+
+import Code from './home-list-content/Code';
+import Music from './home-list-content/Music';
+import Photography from './home-list-content/Photography';
+// import backgroundImage from '../../assets/images/homepage.jpg';
 
 export default function Home() {
-  return (
-    <div className='home-page'>
-        <div className='home-image'>
-            <img src={backgroundImage} alt='homepage'></img>
+    
+    function PortfolioList({ handleContentChange }) {
+        return (
+            <ul>
+                <li>
+                    <h3
+                    onClick={() => handleContentChange('Code')}
+                    >
+                    CODE
+                    </h3>
+                </li>
+                <li>
+                    <h3
+                    onClick={() => handleContentChange('Photography')}
+                    >
+                    PHOTOGRAPHY
+                    </h3>
+                </li>
+                <li>
+                    <h3
+                    onClick={() => handleContentChange('Music')}
+                    >
+                    MUSIC
+                    </h3>
+                </li>
+            </ul>
+        )
+    }
+
+    const [currentContent, setCurrentContent] = useState('PortfolioList');
+    
+    const renderContent = () => {
+        //switch statement for handling portfolio content display
+        switch (currentContent) {
+            case 'Code':
+                return <Code />;
+            case 'Photography':
+                return <Photography />;
+            case 'Music':
+                return <Music />;
+            default:
+                return <PortfolioList />;
+        }
+    }
+
+    const handleContentChange = (content) => setCurrentContent(content);
+
+    return (
+        <div className='home-page'>
+            <div currentContent={currentContent} handleContentChange={handleContentChange}/>
+            {renderContent()}
         </div>
-    </div>
-  );
+    );
 }
