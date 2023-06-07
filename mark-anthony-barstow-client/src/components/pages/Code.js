a3cbb2c8f7a74989d582bb86da2f0d9320243716
Mark Barstow <114883447+marchetype@users.noreply.github.com>
2023-06-07 12:02:55 -0400

    started work on code section of page

--- mark-anthony-barstow-client/src/components/pages/Code.js
+++ mark-anthony-barstow-client/src/components/pages/Code.js
@@ -5,7 +5,14 @@ export default function Code() {
         <div className='code-section'>
             <aside className='external-links'>
             {/* this aside section will provide links to GitHub, other code-related sites */}
-
+            <div
+            className='github'>
+                {/* GH Logo element goes here */}
+                <a
+                href='https://github.com/marchetype' target='_blank' rel='noreferrer'>
+                    marchetype
+                </a>
+            </div>
             </aside>
             <main className='code-highlights'>
             {/* the main section will include all latest projects in a SCROLLABLE GRID */}
