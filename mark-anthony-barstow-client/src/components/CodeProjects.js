de1dc699fb024457b6a7c4dafd535bbb29ceb163
Mark Barstow <114883447+marchetype@users.noreply.github.com>
2023-06-12 14:28:54 -0400

    basic layout for Code section of website is complete

---
+++ mark-anthony-barstow-client/src/components/CodeProjects.js
@@ -0,0 +1,63 @@
+import React from "react";
+
+import photo0 from '../assets/images/codeImages/photo0.png';
+import photo1 from '../assets/images/codeImages/photo1.png';
+import photo2 from '../assets/images/codeImages/photo2.png';
+
+
+export default function RenderCodeProjects() {
+    const projectsArr = [
+        {
+            id: 0,
+            thumbnail: photo0,
+            img_alt: 'Deployed Weather Application',
+            title: 'City Weather Debrief',
+            description: 'This application uses OpenWeatherAPI to display weather data on a retro-inspired User Interface.',
+            link:'https://marchetype.github.io/City_Weather_Debrief/',
+            gh_link:'https://github.com/marchetype/City_Weather_Debrief',
+        },
+        {
+            id: 1,
+            thumbnail: photo1,
+            img_alt: 'id 1',
+            title: 'The Box',
+            description: 'This Application provides a charming experience using ShibeAPI. Visit this sight to gain inspiration for your day!',
+            link:'https://marchetype.github.io/the-box-project/',
+            gh_link: 'https://github.com/marchetype/the-box-project',
+        },
+        {
+            id: 2,
+            thumbnail: photo2,
+            img_alt: 'id 1',
+            title: 'Characterize This!',
+            description: 'Tabletop character generator made using Node, Sequelize, Express, Handlebars.js, Tailwind CSS.',
+            link:'https://characterize-this.herokuapp.com/',
+            gh_link: 'https://github.com/samocain93/character-generator',
+        },
+    ]
+    
+    
+    //rendering projects to the page from projectsArr
+    const codeProjects = projectsArr.map(project =>
+        <section className='col-4'>
+                <div className='card' key={project.id}>
+                        <img className='card-img-top' src={project.thumbnail} alt={project.img_alt}/>
+
+                    
+                    <div className='card-body'>
+                        <h5 className='card-title'>{project.title}</h5>
+                        <p className='card-text'>{project.description}</p>
+                        <a href={project.gh_link} target='_blank' rel='noreferrer' className='btn btn-primary'>App Link</a>
+                        <a href={project.gh_link} target='_blank' rel='noreferrer' className='btn btn-secondary'>GitHub</a>
+                    </div>
+                </div>
+        </section>
+    )
+    return(
+    <main className='col-12 col-md-9 container code-projects'>
+        <div className='row'>
+            {codeProjects}
+        </div>
+    </main>
+    );
+}
--- mark-anthony-barstow-client/src/components/pages/Code.js
+++ mark-anthony-barstow-client/src/components/pages/Code.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import ghLogo from '../../assets/images/github-sign.png'
+import RenderCodeProjects from '../CodeProjects';
 
 export default function Code() {
     return (
         <div className='container-fluid'>
-                <aside className='sm-col-12 md-col-2'>
+            <div className='row'>
+                <aside className='col-12 col-md-3'>
                 {/* this aside section will provide links to GitHub, other code-related sites */}
                     <nav className=''>
                         <h4>External Links</h4>
-                        <div className=''>
+                        <div className='container-fluid'>
                             <a
-                            href='https://github.com/marchetype' target='_blank' rel='noreferrer' className='d-flex flex-row align-items-center'>
+                            href='https://github.com/marchetype' target='_blank' rel='noreferrer' className='row align-items-center'>
                             {/* GH Logo element goes here */}
-                            <img src={ghLogo} alt='GitHub Logo' className='mini-logo'/>
-                            <h5>marchetype</h5>
+                            <img src={ghLogo} alt='GitHub Logo' className='mini-logo-gh col-2'/>
+                            <h5 className='col-2'>marchetype</h5>
                             </a>
                         </div>
                     </nav>
                 </aside>
-            <main className='col-'>
-            {/* the main section will include all latest projects in a SCROLLABLE GRID */}
 
-            </main>
+                {RenderCodeProjects()}
+        
+            </div> 
         </div>
     );
 }
